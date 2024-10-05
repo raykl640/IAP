e5acd30d39d7e1af5ecff0c01523b325dbd53cd6
raylukorito <raylukorito@gmail.com>
2024-10-05 19:38:51 +0300

    "Added UserManagement class with constructor to establish PDO connection"

---
+++ UserManagement.php
@@ -0,0 +1,13 @@
+<?php
+
+class UserManagement {
+    private $pdo;
+
+    public function __construct($host, $db, $user, $pass) {
+        $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        try {

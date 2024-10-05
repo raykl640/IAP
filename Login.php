2af0e6a7382c39179ad0dc8a0c719ba06fb6ff44
raylukorito <raylukorito@gmail.com>
2024-10-05 20:26:01 +0300

    "Added conditional statement to handle POST requests and retrieve username and password from $_POST"

--- Login.php
+++ Login.php
@@ -4,3 +4,6 @@ require_once 'UserManagement.php';
 
 $message = '';
 $userManagement = new UserManagement('localhost', 'user_management', 'root', '');
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
